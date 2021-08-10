415f7d9ea5d18e8e0436a3ca9952f142179b52a7
Mateusz_Prokop <mateusz.prokopp@gmail.com>
2021-08-10 11:26:46 +0200

    Added tooltip feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('test23');
+import Tooltip from './ninja-ui/tooltip';
+
+// create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
--- webpack.config.js
+++ webpack.config.js
@@ -11,19 +11,21 @@ module.exports = {
     publicPath: '/assets/'
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
+    ]
   }
 };
